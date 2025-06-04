a617353c866f5aab08b0ba21c7f350222f72d68a
samco-mokaya <gmansesco@gmail.com>
2025-06-04 19:24:02 +0300

    Created the app and pushed the inital commit

--- src/App.js
+++ src/App.js
@@ -3,23 +3,18 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <Text display="wagwaan"/>
+    <Text display="brudda"/>
+    <p>Hello</p>
+    </>
   );
 }
-
+function Text({display}){
+  return (
+    <div>
+      <p>{display}</p>
+    </div>
+  )
+}
 export default App;

f943ca66286676314e96adf837b337a09cf605ec
samco-mokaya <gmansesco@gmail.com>
2025-06-05 15:33:57 +0300

    Created the components,added the movie card components and routed the pages

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
 
 import './App.css';
 import MovieCard from './components/MovieCard';
-
+import Home from './pages/home';
 function App() {
   return (
     <>
-  <MovieCard movie={{title : "somesonsam",release_date:"2025"}}/>
-  <MovieCard movie={{title: "jojo bizzare adventures",release_date:"2023 "}}/>
+  <Home/>
     </>
   );
 }
---
+++ src/pages/home.jsx
@@ -0,0 +1,30 @@
+import { useState } from "react";
+import MovieCard from "../components/MovieCard"
+
+function Home(){
+
+    const [searchQuery,setSearchQuery]=useState("");
+
+    const movies = [
+        {id:1,title:"JOhn Wick",release_date:"2020"},
+        {id:2,title:"man down",release_date:"2023"},
+        {id:3,title:"Himano",release_date:"2024"},
+
+    ];
+    const handleSearch = () =>{};
+
+    return (
+        <div className="home">
+            <form>
+                <input type="text" placeholder="Search for movies..." className="Search-input" value={searchQuery}/>
+                <button type="submit" className="search-button">Search</button>
+            </form>
+            <div className="movie-grid">
+            {movies.map((movies) => (
+                <MovieCard movie={movies} key={movies.id}/>
+            ))}
+            </div>
+        </div>
+    );
+}
+export default Home

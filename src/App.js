f0e3daf7d8bf7da3b25d1bf81b9338b941df87d3
samco-mokaya <gmansesco@gmail.com>
2025-06-05 21:51:36 +0300

    CReated the Navbar components and added the routes and Links

--- src/App.js
+++ src/App.js
@@ -3,15 +3,20 @@ import './App.css';
 import Home from './pages/home';
 import  {Routes,Route} from "react-router-dom"
 import Favorites from './components/Favorites';
+import NavBar from './components/Navbar';
+
 function App() {
   return (
-   <main className="main-content">
-   <Routes>
-   <Route path='/' element={<Home/>}/>
-   <Route path='/' element={<Favorites/>}/>
-   </Routes>
+    <div>
+     <NavBar/>
+<main className="main-content">
+  <Routes>
+    <Route path='/' element={<Home/>}/>
+    <Route path='/' element={<Favorites/>}/>
+  </Routes>
+    </main>
+  </div>
    
-   </main>
   );
 }
 export default App;
